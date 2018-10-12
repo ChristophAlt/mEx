0a9bde0e64e3fc9a44d782e8df3e6ab945699f7f
Christoph Alt <christoph.alt@dfki.de>
2018-10-12 11:17:16 +0200

    Added comments and attributes to brat output - add umls candidates as comments - add negations and umls candidates as attributes

--- macss_medical_ie/utils/document_helper.py
+++ macss_medical_ie/utils/document_helper.py
@@ -1,7 +1,10 @@
+import json
+
+
 def doc_to_brat(doc):
     def get_or_create(item2id, item, prefix):
         if item not in item2id:
-            item2id[item] = '{}-{}'.format(prefix, len(item2id) + 1)
+            item2id[item] = '{}{}'.format(prefix, len(item2id) + 1)
         return item2id[item]
 
     entity2id = {}
@@ -10,30 +13,39 @@ def doc_to_brat(doc):
 
     entities = []
     relations = []
+    attributes = []
+    comments = []
+
+    linked_entities = dict(doc._.ents_linked)
 
     relation_idx = 1
+    attribute_idx = 1
     for ent in doc.ents:
         entity_id = get_or_create(entity2id, ent, prefix='T')
         entity = (entity_id, ent.label_.upper(), [[ent.start_char, ent.end_char]], ent.text)
-
-        # if an entity is negated, we create a 'negation' entity from the negation source and also add an arc between them
-        if ent._.is_negated:
-            negation_source = ent._.negation_source
-
-            negation_entity_id = get_or_create(entity2id, negation_source, prefix='T')
-            negation_entity = (negation_entity_id, 'NEGATION', [[negation_source.start_char, negation_source.end_char]], negation_source.text)
-            entities.append(negation_entity)
-
-            relation = ['R%d' % relation_idx, 'negates', [['', negation_entity_id], ['', entity_id]]]
-            relations.append(relation)
-            relation_idx += 1
         entities.append(entity)
 
-    for entity_tail, entity_head, relation in doc._.rels:
+        # create attributes for negated entities
+        if ent._.is_negated:
+            attributes.append(('A%d' % attribute_idx, 'Negation', entity_id))
+            attribute_idx += 1
+
+        if ent in linked_entities:
+            cuis = [cui for cui, _ in sorted(linked_entities[ent].items(), key=lambda e: -e[1])]
+
+            attributes.append(('A%d' % attribute_idx, 'UMLSCandidate', entity_id))
+            attribute_idx += 1
+
+            # TODO: this naming scheme and format should be refactored
+            umls_candidates = [{'CUI': cui} for cui in cuis]
+            comments.append((entity_id, json.dumps({'String': ent.text, 'UMLS-Candidates': umls_candidates})))
+
+
+    for entity_tail, entity_head, relation_name in doc._.rels:
         entity_tail_id = get_or_create(entity2id, entity_tail, prefix='T')
         entity_head_id = get_or_create(entity2id, entity_head, prefix='T')
 
-        relation = ('R%d' % relation_idx, relation, [['', entity_tail_id], ['', entity_head_id]])
+        relation = ('R%d' % relation_idx, relation_name, [['', entity_tail_id], ['', entity_head_id]])
 
         relations.append(relation)
         relation_idx += 1
@@ -41,5 +53,7 @@ def doc_to_brat(doc):
     return {
             'text': doc.text,
             'entities': entities,
-            'relations': relations
+            'relations': relations,
+            'comments': comments,
+            'attributes': attributes
         }
